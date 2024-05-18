6d0ca12df49529d820a19a2e4cf383f436a9c4ce
Noé Steiner <72690981+unshade@users.noreply.github.com>
2024-05-18 14:36:46 +0200

    FEAT : Binarized labels

--- train.py
+++ train.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
 dataToTrain = pd.read_csv('./data/sign_mnist_train/sign_mnist_train.csv')
 dataToTest = pd.read_csv('./data/sign_mnist_test/sign_mnist_test.csv')
 alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXY'
 
-print("-- DATA TO BE TRAINED --")
 #print(dataToTrain)
-print("-- DATA TO BE TESTED --")
 #print(dataToTest)
 
 
-print("-- POSSIBLE UNIQUE LABELS --")
 labels = dataToTrain['label'].values
 unique_labels = np.sort(pd.unique(labels))
 mapping = dict(zip(unique_labels, alphabet))
-print(mapping)
 
 # Prepare data for training
 # Drop the label column from the training data
 trainingImages = dataToTrain.drop('label', axis=1).values
-# Each image is 28x28 pixels
-trainingImages = [np.reshape(image, (28, 28)) for image in trainingImages]
+
+# Binarize the labels
+labels = LabelBinarizer().fit_transform(labels)
