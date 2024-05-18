6aafa54312629a0d3212eae3b10a2741691e317d
Noé Steiner <72690981+unshade@users.noreply.github.com>
2024-05-18 13:47:13 +0200

    FEAT : Extract labels and mapp them

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import numpy as np
 
 dataToTrain = pd.read_csv('./data/sign_mnist_train/sign_mnist_train.csv')
 dataToTest = pd.read_csv('./data/sign_mnist_test/sign_mnist_test.csv')
+alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXY'
 
-print("-- THIS IS THE DATA TO BE TRAINED --")
-print(dataToTrain)
-print("-- THIS IS THE DATA TO BE TESTED --")
-print(dataToTest)
+print("-- DATA TO BE TRAINED --")
+#print(dataToTrain)
+print("-- DATA TO BE TESTED --")
+#print(dataToTest)
+
+
+print("-- POSSIBLE UNIQUE LABELS --")
+labels = dataToTrain['label'].values
+unique_labels = np.sort(pd.unique(labels))
+mapping = dict(zip(unique_labels, alphabet))
+print(mapping)
