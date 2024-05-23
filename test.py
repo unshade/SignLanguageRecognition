f11b72cc7e931e37ab43901e04e566706bfbc270
Alexis MARCEL <AlexisMarcel55@gmail.com>
2024-05-23 17:27:33 +0200

    FEAT : print recall and precision

--- test.py
+++ test.py
@@ -2,11 +2,10 @@ import keras
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 
 dataToTest = pd.read_csv('./data/sign_mnist_test/sign_mnist_test.csv')
-
 
 test_labels = dataToTest['label']
 dataToTest.drop('label', axis = 1, inplace = True)
@@ -15,8 +14,11 @@ label_binrizer = LabelBinarizer()
 test_labels = label_binrizer.fit_transform(test_labels)
 test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
 
-model = keras.models.load_model('./model.h5')
+model = keras.models.load_model('./model.keras')
 
 y_pred = model.predict(test_images)
 
 print("Accuracy: ", accuracy_score(test_labels, y_pred.round()))
+
+print("Precision: ", precision_score(test_labels, y_pred.round(), average='weighted'))
+print("Recall: ", recall_score(test_labels, y_pred.round(), average='weighted'))
